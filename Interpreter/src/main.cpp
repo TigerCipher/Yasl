91153749d5295d94ce8a29c927b18604c33be1bf
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2024-06-23 22:04:01 -0500

    moved executable interpreter to cpp

---
+++ Interpreter/src/main.cpp
@@ -0,0 +1,41 @@
+//  ------------------------------------------------------------------------------
+//
+//  Interpreter
+//     Copyright 2024 Matthew Rogers
+//
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
+//
+//  ------------------------------------------------------------------------------
+
+#include <cstdio>
+extern "C" {
+#include "parser.tab.h"
+}
+
+extern FILE* yyin;
+
+int main(int argc, char** argv)
+{
+    if (argc > 1)
+    {
+        FILE* file = fopen(argv[1], "r");
+        if (!file)
+        {
+            perror(argv[1]);
+            return 1;
+        }
+        yyin = file;
+    }
+
+    return yyparse();
+}
